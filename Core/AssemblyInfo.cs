679b5d512507e288a3fad1be21db652c00856d1d
Feryquence <hakutei0527@outlook.com>
2026-09-24 18:11:41 +0800

    Clear files from cards branch

--- Core/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("Air Card")]
-[assembly: AssemblyProduct("Air Card")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: AssemblyInformationalVersion("1.3")]
